a7a99ccd116a843e613b073343a2de4f88dc42cf
NoCream-jp <occ3710@outlook.jp>
2026-03-06 04:25:25 +0900

    fix/ダミーデータの削除

--- src/data/terminalData.ts
+++ src/data/terminalData.ts
@@ -1,5 +1,6 @@
 export const dailyData: { [key: string]: string } = {
-  '2026-03-06': 'It was rainy.',
-  '2026-03-07': 'It was sunny and warm.',
-  '2026-03-08': 'Cloudy with a chance of meatballs.',
+  '2026-03-03': 'Meeting online',
+  '2026-03-04': 'Karaoke !',
+  '2026-03-05': 'Met my friends',
+  '2026-03-06': 'Build terminal in my site !?',
 };
