7a56ac257d04bfba02cdc087b6ee6cf4ce15e76c
NoCream-jp <occ3710@outlook.jp>
2026-03-06 04:19:01 +0900

    fix/データ場所の整理

---
+++ src/data/terminalData.ts
@@ -0,0 +1,5 @@
+export const dailyData: { [key: string]: string } = {
+  '2026-03-06': 'It was rainy.',
+  '2026-03-07': 'It was sunny and warm.',
+  '2026-03-08': 'Cloudy with a chance of meatballs.',
+};
