04025d1d7c6ade9bffc42a0c69a277cbe4581e37
NoCream-jp <occ3710@outlook.jp>
2026-07-16 15:40:38 +0900

    feat: id.astroで数式レンダリングとコンポーネント利用ができるよう改良

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [icon()],
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+    icon(),
+  ],
   site: "https://mi-nato.com",
 });
